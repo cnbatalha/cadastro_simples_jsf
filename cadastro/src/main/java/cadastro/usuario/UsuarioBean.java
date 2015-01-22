547f1da9c631f09b47170846c6ede1e20fe086d4
Carlos Nascimento <cnbatalha@gmail.com>
2015-01-22 09:23:00 -0400

    - Comunicando com banco de dados

--- cadastro/src/main/java/cadastro/usuario/UsuarioBean.java
+++ cadastro/src/main/java/cadastro/usuario/UsuarioBean.java
@@ -1,37 +1,52 @@
 package cadastro.usuario;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
-
-@ManagedBean( name = "usuarioBean")
+@ManagedBean(name = "usuarioBean")
 @SessionScoped
 public class UsuarioBean {
 
 	private Usuario usuario = new Usuario();
-	
-	public String novo()
-	{
+	private List<Usuario> lista;
+
+	public String novo() {
 		this.usuario = new Usuario();
 		return "usuario";
 	}
-		
-	public String salvar()
-	{
+
+	public String salvar() {
 		FacesContext context = FacesContext.getCurrentInstance();
-		
+
 		UsuarioRN usuarioRN = new UsuarioRN();
 		usuarioRN.salvar(usuario);
+
+		this.lista = usuarioRN.listar();
 		
 		return "usuarioSucesso";
 	}
-	
+
+	public String gerenciar() {
+		return "gerenciarUsuario";
+	}
+
 	public Usuario getUsuario() {
 		return usuario;
 	}
-	
+
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
+
+	public List<Usuario> getLista() {
+		if (this.lista == null) {
+			UsuarioRN usuarioRN = new UsuarioRN();
+			this.lista = usuarioRN.listar();
+		}
+		return lista;
+	}
+
 }
